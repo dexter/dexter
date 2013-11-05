f51d16c8425ceb6ce3aa54fc0f974f824fec09e9
diegoceccarelli <diego.ceccarelli@gmail.com>
2013-11-05 23:30:03 +0100

    generate incoming / outcoming categories

--- dexter-core/src/main/java/it/cnr/isti/hpc/dexter/cli/categories/ExtractCategoryEdgesCLI.java
+++ dexter-core/src/main/java/it/cnr/isti/hpc/dexter/cli/categories/ExtractCategoryEdgesCLI.java
@@ -70,7 +70,9 @@ public class ExtractCategoryEdgesCLI extends AbstractCommandLineInterface {
 			for (Link c : a.getCategories()){
 				logger.info("id {}", c.getCleanId());
 				int target = helper.getId(c.getCleanId());
+				logger.info("target {}", target);
 				if (target <= 0) continue;
+				
 				cli.writeLineInOutput(a.getType()+"\t"+id+"\t"+target);	
 			}
 			
