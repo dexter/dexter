91879ebcd0b97915069cd3a4ecefe0d862d40da6
diegoceccarelli <diego.ceccarelli@gmail.com>
2013-11-05 23:00:14 +0100

    generate incoming / outcoming categories

--- dexter-core/scripts/generate-incoming-and-outcoming-categories.sh
+++ dexter-core/scripts/generate-incoming-and-outcoming-categories.sh
@@ -4,29 +4,6 @@ source scripts/config.sh
 
 
 echo "extracting spots for categories"
-$JAVA it.cnr.isti.hpc.dexter.cli.spot.ExtractSpotsCLI --input $WIKI_CATEGORIES --output $TMP
-echo "sorting categories spots by text and target category"
-cat $TMP | sort -t'	' -k1,1 -k3,3n  | uniq > $TTMP
-echo "filtering edges"
-cat $TTMP | awk -F'	' '{if ($2 > 0 && $3 > 0) print $2"	"$3}'  | sort -nk2,2 -nk1,1 | uniq > $TMP
-echo "generating incoming categories file in $IN_CATEGORIES"
-awk -F'	' 'BEGIN{current=$2; incoming=""} {if ($1 != $2) if ($2 == current) {incoming=incoming$1" "} else { print current"\t"incoming; current=$2; incoming=$1" "} } END {print current"\t"incoming}' $TMP  > $TTMP
-
-#first line is empty, i need to remove it 
-sed 1d $TTMP > $TTTMP
-
-echo "compressing"
-gzip $TTTMP
-mv $TTTMP.gz $IN_CATEGORIES
-
-echo "sorting by source"
-sort -nk1,1 -nk2,2 $TMP > $TTMP
-echo "generating outcoming categories file in $OUT_CATEGORIES"
-awk -F'	' 'BEGIN{current=$1; incoming=""} {if ($1 != $2) if ($1 == current) {incoming=incoming$2" "} else { print current"\t"incoming; current=$1; incoming=$2" "} } END {print current"\t"incoming}' $TTMP  > $TMP
-
-sed 1d $TMP > $TTMP
-gzip $TTMP 
-echo "compressing"
-mv $TTMP.gz $OUT_CATEGORIES
-rm $TMP
+$JAVA $CLI.categories.ExtractCategoryEdgesCLI --input $WIKI_CATEGORIES --output $TMP
+echo "done in $TMP"
 
