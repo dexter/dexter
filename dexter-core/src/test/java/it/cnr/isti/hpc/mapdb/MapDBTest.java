4eb819133fea6e1922090347f3a2e9e2af226bcd
diegoceccarelli <diego.ceccarelli@gmail.com>
2013-11-18 19:49:44 +0100

    added readonly mode in mapdb

--- dexter-core/src/test/java/it/cnr/isti/hpc/mapdb/MapDBTest.java
+++ dexter-core/src/test/java/it/cnr/isti/hpc/mapdb/MapDBTest.java
@@ -31,7 +31,7 @@
  */
 package it.cnr.isti.hpc.mapdb;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.io.File;
 import java.io.IOException;
@@ -42,28 +42,26 @@ import org.junit.Test;
 /**
  * @author Diego Ceccarelli <diego.ceccarelli@isti.cnr.it>
  * 
- * Created on Aug 25, 2013
+ *         Created on Aug 25, 2013
  */
 public class MapDBTest {
 
 	@Test
 	public void test() throws IOException {
 		File test = File.createTempFile("mapdbtest-", ".db");
-		MapDB mapdb = new MapDB(test);
+		MapDB mapdb = new MapDB(test, false);
 		Map collection = mapdb.getCollection("test");
 		collection.put("diego", 1);
-		collection.put("salvo",2);
+		collection.put("salvo", 2);
 		collection.put("dexter", 3);
 		mapdb.commit();
 		mapdb.close();
 		mapdb = new MapDB(test);
 		collection = mapdb.getCollection("test");
-		assertEquals(1,collection.get("diego"));
-		assertEquals(2,collection.get("salvo"));
-		assertEquals(3,collection.get("dexter"));
-		
-		
-		
+		assertEquals(1, collection.get("diego"));
+		assertEquals(2, collection.get("salvo"));
+		assertEquals(3, collection.get("dexter"));
+
 	}
 
 }
