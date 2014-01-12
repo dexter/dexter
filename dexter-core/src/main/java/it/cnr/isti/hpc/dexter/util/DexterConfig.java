e4d3d81e0945b72e30f07ce252e08502b8222159
diegoceccarelli <diego.ceccarelli@gmail.com>
2014-01-12 16:28:25 +0100

    add xml configuration file and parser

--- dexter-core/src/main/java/it/cnr/isti/hpc/dexter/util/DexterConfig.java
+++ dexter-core/src/main/java/it/cnr/isti/hpc/dexter/util/DexterConfig.java
@@ -52,11 +52,18 @@ public class DexterConfig {
 	private static final Logger logger = LoggerFactory
 			.getLogger(DexterConfig.class);
 
-	Models models;
-	Labels labels;
-	Index index;
+	private Models models;
+	private Labels labels;
+	private Index index;
+	private Threshold threshold;
+	private Graphs graphs;
+	private Rankers rankers;
+	private Spots spots;
+	private Libs libs;
+	private RelatednessFunctions relatednessFunctions;
+	private Disambiguators disambiguators;
 
-	public DexterConfig() {
+	private DexterConfig() {
 
 	}
 
@@ -75,7 +82,7 @@ public class DexterConfig {
 
 	}
 
-	public static class Model {
+	private static class Model {
 
 		String name;
 		String path;
@@ -87,25 +94,156 @@ public class DexterConfig {
 
 	}
 
-	public static class Labels {
+	private static class Labels {
 		String dir;
 	}
 
-	public static class Index {
+	private static class Index {
 		String dir;
 		String wikiIdMap;
 	}
 
-	public static void main(String[] args) {
+	private static class Threshold {
+		float commonness;
+		float linkprobability;
+	}
+
+	private static class Graphs {
+		String dir;
+		List<Graph> graphs = new ArrayList<Graph>();
+
+	}
+
+	private static class Graph {
+		String name;
+		String incoming;
+		String outcoming;
+
+	}
+
+	private static class EntityEntity {
+		String incoming;
+		String outcoming;
+	}
+
+	private static class EntityCategory {
+		String incoming;
+		String outcoming;
+	}
+
+	private static class CategoryCategory {
+		String incoming;
+		String outcoming;
+	}
+
+	private static class Rankers {
+		List<Ranker> rankers = new ArrayList<Ranker>();
+	}
+
+	private static class Ranker {
+		String name;
+		String clazz;
+	}
+
+	private static class Spots {
+		String dir;
+	}
+
+	private static class Libs {
+		String lib;
+	}
+
+	private static class RelatednessFunctions {
+		List<RelatednessFunction> relatednessFunctions = new ArrayList<RelatednessFunction>();
+	}
+
+	private static class RelatednessFunction {
+		String name;
+		String clazz;
+	}
+
+	private static class Disambiguators {
+		List<Disambiguator> disambiguators = new ArrayList<Disambiguator>();
+	}
+
+	private static class Spotters {
+		List<Spotter> spotters = new ArrayList<Spotter>();
+	}
+
+	private static class Spotter {
+		String name;
+		String clazz;
+	}
+
+	private static class Disambiguator {
+		String name;
+		String clazz;
+	}
+
+	private static class Taggers {
+		List<Tagger> taggers = new ArrayList<Tagger>();
+	}
+
+	private static class Tagger {
+		String name;
+		String spotter;
+		String disambiguator;
+		String relatedness;
+	}
+
+	public static DexterConfig load(String xmlConfig) {
+		logger.info("loading configuration from {} ", xmlConfig);
 		XStream xstream = new XStream(new StaxDriver());
 		xstream.alias("config", DexterConfig.class);
 		xstream.alias("model", Model.class);
 		xstream.alias("labels", Labels.class);
 		xstream.alias("index", Index.class);
+		xstream.alias("threshold", Threshold.class);
+		xstream.alias("graph", Graph.class);
+		xstream.alias("graphs", Graphs.class);
+		xstream.addImplicitCollection(Graphs.class, "graphs");
 
+		xstream.alias("rankers", Rankers.class);
 		xstream.addImplicitCollection(Models.class, "models");
+		xstream.addImplicitCollection(Rankers.class, "rankers");
+		xstream.alias("ranker", Ranker.class);
+		xstream.aliasField("class", Ranker.class, "clazz");
+
+		xstream.alias("entityEntity", EntityEntity.class);
+		xstream.alias("entityCategory", EntityCategory.class);
+
+		xstream.alias("categoryCategory", CategoryCategory.class);
+		xstream.alias("spots", Spots.class);
+		xstream.alias("libs", Libs.class);
+
+		xstream.addImplicitCollection(RelatednessFunctions.class,
+				"relatednessFunctions");
+		xstream.alias("relatednessFunctions", RelatednessFunctions.class);
+		xstream.alias("relatednessFunction", RelatednessFunction.class);
+
+		xstream.alias("disambiguators", Disambiguators.class);
+		xstream.addImplicitCollection(Disambiguators.class, "disambiguators");
+		xstream.alias("disambiguator", Disambiguator.class);
+
+		xstream.alias("spotters", Spotters.class);
+		xstream.addImplicitCollection(Spotters.class, "spotters");
+		xstream.alias("spotter", Spotter.class);
+
+		xstream.aliasField("class", RelatednessFunction.class, "clazz");
+		xstream.aliasField("class", Disambiguator.class, "clazz");
+		xstream.aliasField("class", Spotter.class, "clazz");
+
+		xstream.alias("taggers", Taggers.class);
+		xstream.addImplicitCollection(Taggers.class, "taggers");
+		xstream.alias("tagger", Tagger.class);
+
 		String xml = IOUtils.getFileAsString("dexter-conf.xml");
 		DexterConfig config = (DexterConfig) xstream.fromXML(xml);
+		return config;
+	}
+
+	public static void main(String[] args) {
+		DexterConfig config = load("dexter-conf.xml");
 		Gson gson = new Gson();
 		System.out.println(gson.toJson(config));
 	}
